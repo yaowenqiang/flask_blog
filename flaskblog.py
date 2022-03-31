ec397a0929c3a457816cd89070efd652354f3aa3
yaowenqiang <yaowenqiang111@163.com>
2022-03-31 23:50:57 +0800

    add template rendering demo

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'jacky yao',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 28, 2022'
+    },
+    {
+        'author': 'jacky yao',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 28, 2022'
+    }
+]
 
 @app.route("/")
 def hello():
     return "<h1>hello world!</h1>"
+
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
